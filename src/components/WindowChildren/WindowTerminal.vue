e450000705ad0b742ee4b3eb57ed4e4659f1e251
l cx <lcx1234lcx1029@outlook.com>
2024-08-11 18:55:31 +0800

    yee

--- src/components/WindowChildren/WindowTerminal.vue
+++ src/components/WindowChildren/WindowTerminal.vue
@@ -62,7 +62,7 @@ export default {
       return this.$store.state.filemap
     },
     header(){
-      let res = "[xuanli@bilibili ~ ]# "
+      let res = "[CX@bilibili ~ ]# "
       for (let dir of this.path_stack) {
         res += (dir + '/')
       }
